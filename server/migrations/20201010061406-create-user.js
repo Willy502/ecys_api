e180b1a1e691590b381f054014b1f4599657f1ed
Alejandro Barrios <wabarrios@digitalgeko.com>
2020-10-10 00:15:15 -0600

    Modelo básico de usuarios

---
+++ server/migrations/20201010061406-create-user.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      carnet: {
+        type: Sequelize.INTEGER
+      },
+      nombres: {
+        type: Sequelize.STRING
+      },
+      apellidos: {
+        type: Sequelize.STRING
+      },
+      contrasenia: {
+        type: Sequelize.STRING
+      },
+      correo: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
