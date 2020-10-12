3713f9d383df70877d8a7f7a964ff7960a744f47
Alejandro Barrios <wabarrios@digitalgeko.com>
2020-10-11 21:39:47 -0600

    Modelo y migracion de cursos

---
+++ server/models/course.js
@@ -0,0 +1,77 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class course extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  course.init({
+    codigo_curso: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      field: 'codigo_curso'
+    },
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "El nombre no pueden estar vacío"
+        },
+        notNull: {
+          msg: "El nombre no pueden ser nulo"
+        }
+      },
+      field:'nombre'
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      validate: {
+        notEmpty: {
+          msg: "created_at no puede estar vacío"
+        },
+        isDate: {
+          msg: "Formato invalido"
+        }
+      },
+      field:'created_at'
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      validate: {
+        notEmpty: {
+          msg: "updated_at no puede estar vacío"
+        },
+        isDate: {
+          msg: "Formato invalido"
+        }
+      },
+      field:'updated_at'
+    }
+  }, {
+    sequelize,
+    timestamps: false, // Eliminamos el updatedAt y createdAt
+    underscored: true, // Habilitar la creacion con guines bajos a nivel de base de datos
+    defaultScope: {
+      attributes: {
+        exclude: [ // excluir campos de un select
+          'created_at',
+          'updated_at'
+        ]
+      }
+    }
+  },  {
+    sequelize,
+    modelName: 'course',
+  });
+  return course;
+};
