b219a9626f4cab584722ab584844ed26fc951a12
Alejandro Barrios <wabarrios@digitalgeko.com>
2020-10-17 22:49:06 -0600

    Login

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,7 @@ const authenticationController = require('../controllers').authentication;
 module.exports = (app) => {
     
     // Authentication routes
-    app.post('/api/users/register', authenticationController.register);
+    app.post('/api/users/register', authenticationController.register); // register
+    app.post('/api/users/login', authenticationController.login); // Login
 
 }
