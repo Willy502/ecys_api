376a65bbfbebe8018b1aad1b1d71eaa3af3810b3
Alejandro Barrios <wabarrios@digitalgeko.com>
2020-10-18 20:31:45 -0600

    Forgot password and update password

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -2,7 +2,6 @@
 const User = require('../models').user;
 // Base Controller
 let { onSuccess, onError } = require('./BaseController');
-const bcrypt = require('bcrypt');
 const { Op } = require('sequelize');
 const jwt = require('jsonwebtoken');
 
@@ -87,7 +86,7 @@ module.exports = {
     // Retrieve somebody's account
     async somebodysAccount(request, response) {
 
-        await User
+        return await User
         .findOne({
             where: {
                 carnet: request.params.carnet
@@ -118,6 +117,88 @@ module.exports = {
                 onError(error, "Falló la operación.", 500)
             );
 
+        });
+    },
+
+    // Get secure token to change password
+    async forgotPassword(request, response) {
+     
+        let body = request.body;
+
+        return await User
+        .findOne({
+            where: {
+                [Op.and]: [
+                    {
+                        correo: body.correo
+                    },
+                    {
+                        carnet: body.carnet
+                    }
+                ]
+            }
+        })
+        .then((result) => {
+
+            if (result != null) {
+
+                delete result.dataValues.contrasenia;
+
+                let token = jwt.sign({
+                    user: result
+                }, process.env.SEED);
+
+                response.status(200).send(
+                    onSuccess({user: result, token}, "Utiliza el token para cambiar de contraseña.", 200)
+                );
+
+            } else {
+                response.status(401).send(
+                    onError(result, "Los datos de usuario no han sido validados.", 401)
+                );
+            }
+
+        })
+        .catch((error) => {
+
+            response.status(500).send(
+                onError(error, "Falló la operación.", 500)
+            );
+
+        });
+
+    },
+
+    // Update password
+    async updatePassword(request, response) {
+
+        let body = request.body;
+
+        return await User
+        .update({
+            contrasenia: body.nombres,
+            updated_at: Date.now()
+        }, {
+            where: {
+                carnet: request.user.carnet
+            }, 
+            returning: true,
+            plain: true
+        })
+        .then((result) => {
+            
+            delete result[1].dataValues.created_at;
+            delete result[1].dataValues.updated_at;
+            delete result[1].dataValues.contrasenia;
+
+            response.status(200).send(
+                onSuccess({user: result[1]}, "Contraseña actualizada exitosamente.", 200)
+            );
+        })
+        .catch((error) => {
+            response.status(500).send(
+                onError(error, 'Falló la operación.', 500)
+            ); 
         });
     },
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -7,12 +7,14 @@ const userController = require('../controllers').user;
 module.exports = (app) => {
     
     // Authentication routes
-    app.post('/api/users/register', authenticationController.register); // register
-    app.post('/api/users/login', authenticationController.login); // Login
+    app.post('/api/user/register', authenticationController.register); // register
+    app.post('/api/user/login', authenticationController.login); // Login
 
     // User routes
     app.get('/api/user', checkToken, userController.account); // Datos de mi cuenta
     app.put('/api/user', checkToken, userController.update); // Actualizar mi cuenta
+    app.get('/api/user/{{carnet}}', checkToken, userController.somebodysAccount); // Datos de la cuenta de X usuario
+    app.post('/api/user/forgot-password', userController.forgotPassword); // Ha olvidado su contraseña
 
 
 }
